bc3fc77b5976c46fbc96827c173bd8dc9234e87f
Ted Casey <tedcasey@adobe.com>
2018-10-31 14:57:56 -0700

    More cleanup of unintentional changes.

--- libraries/ride-fuzzer-lib/src/main/java/com/adobe/ride/libraries/fuzzer/engines/CoreEngine.java
+++ libraries/ride-fuzzer-lib/src/main/java/com/adobe/ride/libraries/fuzzer/engines/CoreEngine.java
@@ -43,9 +43,7 @@ public class CoreEngine extends RideCore {
   private char[] testCharArray = new char[] {'T', 'E', 'S', 'T'};
   private double testDouble = Double.parseDouble("78547893.7583758349753489758934");
   private Number testNumber = 1456.34;
-  private Byte testByte = 1;
-
-  // private byte[] testByteArray = "test string".getBytes();
+  private Byte testByte = 1;\
 
   /**
    * Constructor for the core engine which initializes key members for subclasses.
@@ -72,24 +70,6 @@ public class CoreEngine extends RideCore {
     passiveSQLInjectionFuzzValues = prepDP(passiveSQLInjectionStringsArray, scope, target);
     noSQLInjectionFuzzValues = prepDP(noSQLInjectionStringsArray, scope, target);
     nonBooleanFuzzValues = prepDP(nonBooleanArray, scope, target);
-  }
-
-  /**
-   * Method to populate the request spec with the fuzzer client_id and some default headers
-   * 
-   * @param spec RequestSpecBuilder to be modified and used in the call to the service.
-   * @return
-   */
-  public RequestSpecBuilder initializeFuzzerRequestSpecBldr(RequestSpecBuilder spec) {
-    /*
-     * TODO Header in old DPS framework was replaced by
-     * com.adobe.cloud.automation.core.globals.Headers in core
-     * spec.addHeader(Header.CLIENT_API_KEY.toString(), FUZZER_CLIENT_ID);
-     * spec.addHeader(Header.CLIENT_REQUEST_ID.toString(), Header.CLIENT_REQUEST_ID.defaultValue());
-     * spec.addHeader(Header.CLIENT_VERSION.toString(), Header.CLIENT_VERSION.defaultValue());
-     * spec.addHeader(Header.SESSION_ID.toString(), Header.SESSION_ID.defaultValue());
-     */
-    return spec;
   }
 
   /**
@@ -192,196 +172,196 @@ public class CoreEngine extends RideCore {
 
   @SuppressWarnings("unused")
   private Object[] problemCharsArray = {
-      "///", // invalid location
-      ":::", // improperly placed chars
-      "#%&*{}<>?/+|" // illegal chars
+    "///", // invalid location
+    ":::", // improperly placed chars
+    "#%&*{}<>?/+|" // illegal chars
   };
 
   private Object[] noSQLInjectionStringsArray = {
-      "0;var date=new Date(); do{curDate = new Date();}while(curDate-date<10000)",
-      "$where: function() { for (i = 0; i < 5; i++) {text += 'Failure Loop ' + i + '<br>';} }"
+    "0;var date=new Date(); do{curDate = new Date();}while(curDate-date<10000)",
+    "$where: function() { for (i = 0; i < 5; i++) {text += 'Failure Loop ' + i + '<br>';} }"
     };
 
   private Object[] passiveSQLInjectionStringsArray = {
-      // OWASP recommended Values
-      "1; select * from *.*;",
-      "'test'; select * from *.*;",
-      "'; create table pwned_by_ride (sanitize varchar(20), yo varchar(20), input varchar(20));",
-      "0;var date=new Date(); do{curDate = new Date();}while(curDate-date<10000)",
-      "' SELECT name FROM syscolumns WHERE id = (SELECT id FROM sysobjects WHERE name = tablename')--",
-      "%27+OR+%277659%27%3D%277659",
-      "1;(load_file(char(47,101,116,99,47,112,97,115,115,119,100))),1,1,1;",
-      "'; exec master..xp_cmdshell",
-      // "UNI/**/ON SEL/**/ECT", TODO: Server FGE fails to detect. Disagrees with Sample expectation. Need to investigate. Could be dangerous.
-      "'/**/OR/**/1/**/=/**/1",
-      // comment here to reduce OWASP set
-      "'||(elt(-3+5,bin(15),ord(10),hex(char(45))))",
-      "||6",
-      "'||'6",
-      "(||6)",
-      "' OR 1=1-- ",
-      "OR 1=1",
-      "' OR '1'='1",
-      "; OR '1'='1'",
-      "%22+or+isnull%281%2F0%29+%2F*",
-      "%27+OR+%277659%27%3D%277659",
-      "%22+or+isnull%281%2F0%29+%2F*",
-      "%27+--+",
-      "' or 1=1--",
-      " or 1=1--",
-      "' or 1=1 /*",
-      "or 1=1--",
-      "' or 'a'='a",
-      "') or ('a'='a",
-      // "Admin' OR '" , TODO: Server FGE fails to detect. Should return a 400. Need to investigate. Could be dangerous.
-      ") UNION SELECT%20*%20FROM%20INFORMATION_SCHEMA.TABLES;",
-      "' having 1=1--",
-      "' having 1=1--",
-      "' group by userid having 1=1--",
-      "' or 1 in (select @@version)--",
-      "' union all select @@version--",
-      "' OR 'unusual' = 'unusual'",
-      "' OR 'something' = 'some'+'thing'",
-      "' OR 'text' = N'text'",
-      "' OR 'something' like 'some%'",
-      "' OR 2 > 1",
-      "' OR 'text' > 't'",
-      "' OR 'whatever' in ('whatever')",
-      "' OR 2 BETWEEN 1 and 3",
-      "' or username like char(37);",
-      "' union select * from users where login = char(114,111,111,116);",
-      "' union select ",
-      "'; EXECUTE IMMEDIATE 'SEL' || 'ECT US' || 'ER'",
-      "'; EXEC ('SEL' + 'ECT US' + 'ER')",
-      "' or 1/*",
-      "+or+isnull%281%2F0%29+%2F*",
-      "%22+or+isnull%281%2F0%29+%2F*",
-      "%27+--+&password=",
-      "'; begin declare @var varchar(8000) set @var=':' select @var=@var+'+login+'/'+password+' ' from users where login > ",
-      "@var select @var as var into temp end --",
-      "' and 1 in (select var from temp)--",
-      "' union select 1,load_file('/etc/passwd'),1,1,1;",
-      "' and 1=( if((load_file(char(110,46,101,120,116))<>char(39,39)),1,0));",
-      // Google recommended passive SQL injection
-      // "\\x27\\x4F\\x52 SELECT *" , TODO: Server FGE fails to detect. Disagrees with Sample expectation. Need to investigate. Could be dangerous.
-      // "\\x27\\x6F\\x72 SELECT *" , TODO: ^^
-      "'%20or%20'x'='x",
-      "\"%20or%20\"x\"=\"x",
-      // Comment here to reduce google set
-      "'",
-      "\"",
-      "#",
-      "-",
-      "--",
-      "'%20--",
-      "--';",
-      "'%20;",
-      "=%20'",
-      "=%20;",
-      "=%20--",
-      // "\\x23" , TODO: Server FGE fails to detect. Disagrees with Sample expectation. Need to investigate. Could be dangerous.
-      // "\\x27" , TODO: ^^
-      // "\\x3D%20\\x3B'" , TODO: ^^
-      // "\\x3D%20\\x27" , TODO: ^^
-      "'or%20select *",
-      // "admin'--" , TODO: ^^
-      "<>\"'%;)(&+",
-      "'%20or%20''='",
-      "'%20or%20'x'='x",
-      "\"%20or%20\"x\"=\"x",
-      "')%20or%20('x'='x",
-      "0 or 1=1",
-      "' or 0=0 --",
-      "\" or 0=0 --",
-      "or 0=0 --",
-      "' or 0=0 #",
-      "\" or 0=0 #",
-      "or 0=0 #",
-      "' or 1=1--",
-      "\" or 1=1--",
-      "' or '1'='1'--",
-      "\"' or 1 --'\"",
-      "or 1=1--",
-      "or%201=1",
-      "or%201=1 --",
-      "' or 1=1 or ''='",
-      "\" or 1=1 or \"\"=\"",
-      "' or a=a--",
-      "\" or \"a\"=\"a",
-      "') or ('a'='a", "\") or (\"a\"=\"a",
-      "hi\" or \"a\"=\"a",
-      "hi\" or 1=1 --",
-      "hi' or 1=1 --",
-      "hi' or 'a'='a",
-      "hi') or ('a'='a",
-      "hi\") or (\"a\"=\"a",
-      "'hi' or 'x'='x';",
-      "@variable",
-      ",@variable",
-      // "PRINT" , TODO: Server FGE fails to detect. Disagrees with Sample expectation. Need to investigate. Could be dangerous.
-      // "PRINT @@variable" , TODO: ^^
-      // "select" , TODO: ^^
-      // "insert" , TODO: ^^
-      "as",
-      "or",
-      /*
-       * TODO: Server FGE fails to detect. Disagrees with Sample expectation. Need to investigate. Could be dangerous. "procedure" , "limit" ,
-       * "order by" , "asc" , "desc" , "delete" , "update" , "distinct" , "having" , "truncate" , "replace" , "like" , "handler" , "bfilename" ,
-       */
-      "' or username like '%",
-      "' or uname like '%",
-      "' or userid like '%",
-      "' or uid like '%",
-      "' or user like '%",
-      // "exec xp" , TODO: Server FGE fails to detect. Disagrees with Sample expectation. Need to investigate. Could be dangerous.
-      // "exec sp" , TODO: ^^
-      "'; exec xp_regread",
-      "t'exec master..xp_cmdshell 'nslookup www.google.com'--",
-      "--sp_password"
+    // OWASP recommended Values
+    "1; select * from *.*;",
+    "'test'; select * from *.*;",
+    "'; create table pwned_by_ride (sanitize varchar(20), yo varchar(20), input varchar(20));",
+    "0;var date=new Date(); do{curDate = new Date();}while(curDate-date<10000)",
+    "' SELECT name FROM syscolumns WHERE id = (SELECT id FROM sysobjects WHERE name = tablename')--",
+    "%27+OR+%277659%27%3D%277659",
+    "1;(load_file(char(47,101,116,99,47,112,97,115,115,119,100))),1,1,1;",
+    "'; exec master..xp_cmdshell",
+    // "UNI/**/ON SEL/**/ECT", TODO: Server FGE fails to detect. Disagrees with Sample expectation. Need to investigate. Could be dangerous.
+    "'/**/OR/**/1/**/=/**/1",
+    // comment here to reduce OWASP set
+    "'||(elt(-3+5,bin(15),ord(10),hex(char(45))))",
+    "||6",
+    "'||'6",
+    "(||6)",
+    "' OR 1=1-- ",
+    "OR 1=1",
+    "' OR '1'='1",
+    "; OR '1'='1'",
+    "%22+or+isnull%281%2F0%29+%2F*",
+    "%27+OR+%277659%27%3D%277659",
+    "%22+or+isnull%281%2F0%29+%2F*",
+    "%27+--+",
+    "' or 1=1--",
+    " or 1=1--",
+    "' or 1=1 /*",
+    "or 1=1--",
+    "' or 'a'='a",
+    "') or ('a'='a",
+    // "Admin' OR '" , TODO: Server FGE fails to detect. Should return a 400. Need to investigate. Could be dangerous.
+    ") UNION SELECT%20*%20FROM%20INFORMATION_SCHEMA.TABLES;",
+    "' having 1=1--",
+    "' having 1=1--",
+    "' group by userid having 1=1--",
+    "' or 1 in (select @@version)--",
+    "' union all select @@version--",
+    "' OR 'unusual' = 'unusual'",
+    "' OR 'something' = 'some'+'thing'",
+    "' OR 'text' = N'text'",
+    "' OR 'something' like 'some%'",
+    "' OR 2 > 1",
+    "' OR 'text' > 't'",
+    "' OR 'whatever' in ('whatever')",
+    "' OR 2 BETWEEN 1 and 3",
+    "' or username like char(37);",
+    "' union select * from users where login = char(114,111,111,116);",
+    "' union select ",
+    "'; EXECUTE IMMEDIATE 'SEL' || 'ECT US' || 'ER'",
+    "'; EXEC ('SEL' + 'ECT US' + 'ER')",
+    "' or 1/*",
+    "+or+isnull%281%2F0%29+%2F*",
+    "%22+or+isnull%281%2F0%29+%2F*",
+    "%27+--+&password=",
+    "'; begin declare @var varchar(8000) set @var=':' select @var=@var+'+login+'/'+password+' ' from users where login > ",
+    "@var select @var as var into temp end --",
+    "' and 1 in (select var from temp)--",
+    "' union select 1,load_file('/etc/passwd'),1,1,1;",
+    "' and 1=( if((load_file(char(110,46,101,120,116))<>char(39,39)),1,0));",
+    // Google recommended passive SQL injection
+    // "\\x27\\x4F\\x52 SELECT *" , TODO: Server FGE fails to detect. Disagrees with Sample expectation. Need to investigate. Could be dangerous.
+    // "\\x27\\x6F\\x72 SELECT *" , TODO: ^^
+    "'%20or%20'x'='x",
+    "\"%20or%20\"x\"=\"x",
+    // Comment here to reduce google set
+    "'",
+    "\"",
+    "#",
+    "-",
+    "--",
+    "'%20--",
+    "--';",
+    "'%20;",
+    "=%20'",
+    "=%20;",
+    "=%20--",
+    // "\\x23" , TODO: Server FGE fails to detect. Disagrees with Sample expectation. Need to investigate. Could be dangerous.
+    // "\\x27" , TODO: ^^
+    // "\\x3D%20\\x3B'" , TODO: ^^
+    // "\\x3D%20\\x27" , TODO: ^^
+    "'or%20select *",
+    // "admin'--" , TODO: ^^
+    "<>\"'%;)(&+",
+    "'%20or%20''='",
+    "'%20or%20'x'='x",
+    "\"%20or%20\"x\"=\"x",
+    "')%20or%20('x'='x",
+    "0 or 1=1",
+    "' or 0=0 --",
+    "\" or 0=0 --",
+    "or 0=0 --",
+    "' or 0=0 #",
+    "\" or 0=0 #",
+    "or 0=0 #",
+    "' or 1=1--",
+    "\" or 1=1--",
+    "' or '1'='1'--",
+    "\"' or 1 --'\"",
+    "or 1=1--",
+    "or%201=1",
+    "or%201=1 --",
+    "' or 1=1 or ''='",
+    "\" or 1=1 or \"\"=\"",
+    "' or a=a--",
+    "\" or \"a\"=\"a",
+    "') or ('a'='a", "\") or (\"a\"=\"a",
+    "hi\" or \"a\"=\"a",
+    "hi\" or 1=1 --",
+    "hi' or 1=1 --",
+    "hi' or 'a'='a",
+    "hi') or ('a'='a",
+    "hi\") or (\"a\"=\"a",
+    "'hi' or 'x'='x';",
+    "@variable",
+    ",@variable",
+    // "PRINT" , TODO: Server FGE fails to detect. Disagrees with Sample expectation. Need to investigate. Could be dangerous.
+    // "PRINT @@variable" , TODO: ^^
+    // "select" , TODO: ^^
+    // "insert" , TODO: ^^
+    "as",
+    "or",
+    /*
+     * TODO: Server FGE fails to detect. Disagrees with Sample expectation. Need to investigate. Could be dangerous. "procedure" , "limit" ,
+     * "order by" , "asc" , "desc" , "delete" , "update" , "distinct" , "having" , "truncate" , "replace" , "like" , "handler" , "bfilename" ,
+     */
+    "' or username like '%",
+    "' or uname like '%",
+    "' or userid like '%",
+    "' or uid like '%",
+    "' or user like '%",
+    // "exec xp" , TODO: Server FGE fails to detect. Disagrees with Sample expectation. Need to investigate. Could be dangerous.
+    // "exec sp" , TODO: ^^
+    "'; exec xp_regread",
+    "t'exec master..xp_cmdshell 'nslookup www.google.com'--",
+    "--sp_password"
     };
 
   @SuppressWarnings("unused")
   private Object[] nonIntArray = {
-      testString,
-      true,
-      testObj,
-      // testLong,
-      testDouble,
-      testNumber,
-      testCharArray,
-      testByte,
-      // testByteArray
+    testString,
+    true,
+    testObj,
+    // testLong,
+    testDouble,
+    testNumber,
+    testCharArray,
+    testByte,
+    // testByteArray
   };
 
   private Object[] nonBooleanArray = {
-      testString,
-      -53,
-      53,
-      testObj,
-      // testLong,
-      testDouble,
-      testNumber,
-      testCharArray,
-      testByte,
-      // testByteArray
+    testString,
+    -53,
+    53,
+    testObj,
+    // testLong,
+    testDouble,
+    testNumber,
+    testCharArray,
+    testByte,
+    // testByteArray
   };
 
   private Object[] localizedStringsArray = {
-      // reduced set of localized strings
-      "あじえうぃ", // Hiragana
-      "ｈｅｌｌｏ", // Full-width romaji
-      "اعنتعزر نهتاثس", // Arabic
-      "καλι μερα", // Greek
-      "בישךךשי", // Hebrew
-      "虎劲 挖池内", // Pinyin Simplified
-      "öäåÖÄÅ", // High-ascii chars
-      "フジウイイベ", // Katakana
-      "ﾌｼﾞｲｳﾞｧ", // Half-width katakana
-      "エヌヂオアバ", // Ainu
-      "اعثمهتسةث", // Arabic-PC
-      "沁 秋", // Wubi Xing
-      "ㄊㄧ ㄕ ㄙㄠ", // Zhuyin
-      "門佃" // Cangjie
+    // reduced set of localized strings
+    "あじえうぃ", // Hiragana
+    "ｈｅｌｌｏ", // Full-width romaji
+    "اعنتعزر نهتاثس", // Arabic
+    "καλι μερα", // Greek
+    "בישךךשי", // Hebrew
+    "虎劲 挖池内", // Pinyin Simplified
+    "öäåÖÄÅ", // High-ascii chars
+    "フジウイイベ", // Katakana
+    "ﾌｼﾞｲｳﾞｧ", // Half-width katakana
+    "エヌヂオアバ", // Ainu
+    "اعثمهتسةث", // Arabic-PC
+    "沁 秋", // Wubi Xing
+    "ㄊㄧ ㄕ ㄙㄠ", // Zhuyin
+    "門佃" // Cangjie
   };
   // @formatter:on
 }
